a1e2574c80e98721e2c50c799cd64fdf81d334d6
ryuya-yanagi <79124542+ryuya-yanagi@users.noreply.github.com>
2021-03-25 10:29:19 +0900

    fix: オーバーレイクリック時にモダールを閉じる

--- practice/03/try/src/js/index.js
+++ practice/03/try/src/js/index.js
@@ -1,21 +1,33 @@
 (function () {
-  'use strict';
+  "use strict";
 
-  const button = document.getElementById('button')
-  const modalOverlay = document.getElementById('modal-overlay')
-  const modalContent = document.getElementById('modal-content')
-  const modalClose = document.getElementById('modal-close')
+  const button = document.getElementById("button");
+  const modalOverlay = document.getElementById("modal-overlay");
+  const modalContent = document.getElementById("modal-content");
+  const modalClose = document.getElementById("modal-close");
 
   // classList.add, removeを使用した実装
-  button.addEventListener('click', () => {
-    modalOverlay.classList.add('is-active')
-    modalContent.classList.add('is-active')
-  })
-  
-  modalClose.addEventListener('click', () => {
-    modalOverlay.classList.remove('is-active')
-    modalContent.classList.remove('is-active')
-  })
+  button.addEventListener("click", () => {
+    addActiveClass();
+  });
+
+  modalClose.addEventListener("click", () => {
+    removeActiveClass();
+  });
+
+  modalOverlay.addEventListener("click", () => {
+    removeActiveClass();
+  });
+
+  function addActiveClass() {
+    modalOverlay.classList.add("is-active");
+    modalContent.classList.add("is-active");
+  }
+
+  function removeActiveClass() {
+    modalOverlay.classList.remove("is-active");
+    modalContent.classList.remove("is-active");
+  }
 
   /*
   ** classList.toggleを使用した実装 **
@@ -30,4 +42,4 @@
   button.addEventListener('click', toggleActive)
   modalClose.addEventListener('click', toggleActive)
   */
-})()
+})();
